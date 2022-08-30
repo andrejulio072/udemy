2c16fc74da511e99fcb1abc9a896cac5787bfcb9
Andre Garcia <andrejulio072@hotmail.com>
2022-08-30 13:09:35 +0100

    leetocde

---
+++ react/fundamentos-react/src/components/basics/Comparametro.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Comparametros(props) {
+  return <div>
+    <h2>{props.titulo}</h2>
+    <h3>{props.subtitulo}</h3>
+  </div>;
+}
--- react/fundamentos-react/src/index.js
+++ react/fundamentos-react/src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Primeiro from "./components/basics/Primeiro";
+import ComParametro from "./components/basics/ComParametro";
 
 
 
 ReactDOM.render(
-  <div>
+  <div id='app'>
     <Primeiro />
+    <ComParametro
+    titulo="segundo componente"
+    subtitulo="Muito Legal"
+     valor="Oi" />
   </div>,
   document.getElementById("root")
 );
