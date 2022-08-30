30e1289bb34f443b8592958cbfdbf2fc91eafbb6
Andre Garcia <andrejulio072@hotmail.com>
2022-08-30 13:02:30 +0100

    leetocde

---
+++ react/fundamentos-react/src/components/basics/Primeiro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Primeiro() {
+  const msg= "Seja bem-vindo(a) ao curso de React";
+  return (
+    <div>
+      <h1>Primeiro Component</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
