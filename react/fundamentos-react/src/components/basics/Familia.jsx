a240b216e0b493e8b9179a1e86c842970adea6ea
Andre Garcia <andrejulio072@hotmail.com>
2022-08-31 13:40:10 +0100

    udemy react

--- react/fundamentos-react/src/components/basics/Familia.jsx
+++ react/fundamentos-react/src/components/basics/Familia.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { cloneElement } from "react";
 
 export default function Familia(props) {
   return (
     <div>
-      { props.children }
+      {React.Children.map(props.children, (child) => {
+        return cloneElement(child, props);
+      })}
     </div>
   );
 }
