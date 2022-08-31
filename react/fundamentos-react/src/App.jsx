02386a04020edf9384bf55ed0519227f04a24a21
Andre Garcia <andrejulio072@hotmail.com>
2022-08-31 13:48:43 +0100

    udemy react

--- react/fundamentos-react/src/App.jsx
+++ react/fundamentos-react/src/App.jsx
@@ -10,17 +10,12 @@ import Familia from "./components/basics/Familia";
 import FamiliaMembro from "./components/basics/FamiliaMembro";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
   <div className="App">
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
-      <Card titulo="#13 - Desafio Repetição" color="#3A9AD9">
-        <ListaAlunos />
-      </Card>
-      <Card titulo="#08 - Renderização Condicional" color="#008BBA"></Card>
-
-      <Card titulo="#07 - Desafio Repetição" color="#D96459"></Card>
 
       <Card titulo="#06 - Repetição" color="#FF85CB">
         <ListaAlunos />
@@ -33,6 +28,7 @@ export default () => (
           <FamiliaMembro nome="Gustavo" />
         </Familia>
       </Card>
+
       <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
         <Aleatorio min={1} max={60} />
       </Card>
