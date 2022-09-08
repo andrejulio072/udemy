3842e554f380f3781ea60c5ae5fab2dab198b89d
Andre Garcia <andrejulio072@hotmail.com>
2022-09-08 10:00:39 +0100

    react

--- react/fundamentos-react/src/components/comunicacao/DiretaPai.jsx
+++ react/fundamentos-react/src/components/comunicacao/DiretaPai.jsx
@@ -4,7 +4,8 @@ import DiretaFilho from './DiretaFilho';
 export default props => {
     return (
         <div>
-            <DiretaFilho texto='Filho' numero={20} bool={true} />
+            <DiretaFilho nome='Junior' idade={20} nerd={true} />
+            <DiretaFilho nome='Gabriel' idade={28} nerd={false} />
         </div>
     )
 }
--- react/fundamentos-react/src/components/condicional/UsuarioInfo.jsx
+++ react/fundamentos-react/src/components/condicional/UsuarioInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import If, { Else } from "./If";
+import If from "./If";
 
 export default (props) => {
   return (
     <div>
       <If test={props.usuario && props.usuario.nome}>
-      Seja bem vindo <strong>{props.usuario.nome}</strong>!
-    </If>
+        Seja bem vindo <strong>{props.usuario.nome}</strong>!
+      </If>
     </div>
   );
 };
