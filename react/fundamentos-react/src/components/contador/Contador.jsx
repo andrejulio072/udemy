d93789cb34dde5f4da3935bf22b002de66d40f94
Andre Garcia <andrejulio072@hotmail.com>
2022-09-10 21:06:53 +0100

    react

---
+++ react/fundamentos-react/src/components/contador/Contador.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Contador extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <h2>Contador</h2>
+      </div>
+    );
+  }
+}
+
+export default Contador
