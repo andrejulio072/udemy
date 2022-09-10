5e1bc1a165468891bd18c67520115a8d0baf6a37
Andre Garcia <andrejulio072@hotmail.com>
2022-09-10 20:47:41 +0100

    react

--- react/fundamentos-react/src/components/formulario/Input.jsx
+++ react/fundamentos-react/src/components/formulario/Input.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default (props) => {
+
+  const [valor, setValor] = React.useState("Inicial");
+
   return <div>Input</div>;
 };
