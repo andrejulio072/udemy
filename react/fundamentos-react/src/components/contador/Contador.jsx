89e0cb49d70aab70cb5f0786b7cf89484741c2f3
Andre Garcia <andrejulio072@hotmail.com>
2022-09-10 21:15:52 +0100

    react

--- react/fundamentos-react/src/components/contador/Contador.jsx
+++ react/fundamentos-react/src/components/contador/Contador.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 class Contador extends React.Component {
+  constructor(props) {
+    super(props);
 
-constructor(props) {
-  super(props);
-
-  this.state = {
-    numero: this.props.numeroInicial || 0,
+    this.state = {
+      numero: this.props.numeroInicial || 0,
+    };
+  }
+  incrementar = () => {
+    this.setState({ numero: this.state.numero + 1 });
   };
-}
 
   render() {
     return (
